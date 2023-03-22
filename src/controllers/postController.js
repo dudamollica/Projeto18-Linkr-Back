434691e32ab6034d8cad85224f9fdcaca5fc840e
dudamollica <dudarenno@yahoo.com.br>
2023-03-22 15:37:58 -0300

    Feat: Delete Post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,4 +1,4 @@
-import { addLike, sumLikesAmount } from "../repositories/postRepository.js";
+import { addLike, sumLikesAmount, postDelete, postCreator} from "../repositories/postRepository.js";
 
 export async function likePost(req, res) {
   const { id } = req.params;
@@ -21,6 +21,18 @@ export async function amountOfLikes(req, res) {
   }
 }
 
-export async function deletePost(req, res) {}
+export async function deletePost(req, res) {
+  const post_id = req.params.id;
+  const userId = res.locals.user;
+  try {
+    const userCreator = await postCreator(post_id)
+    if(userCreator.rows[0] == userId){
+    await postDelete(post_id);
+    res.status(200).send("ok");
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
 
 export async function editPost(req, res) {}
--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -1,9 +1,20 @@
-import db from "../config/database.js"
+import db from "../config/database.js";
 
-export async function addLike({id, userId}){
-  return await db.query(`INSERT INTO likes (post_id, user_id) VALUES (${id}, ${userId})`)
+export async function addLike({ id, userId }) {
+  return await db.query(
+    `INSERT INTO likes (post_id, user_id) VALUES (${id}, ${userId})`
+  );
 }
 
-export async function sumLikesAmount(post_id){
-    return await db.query(`SELECT count(*) FROM likes WHERE post_id=${post_id}`)
-  }
+export async function sumLikesAmount(post_id) {
+  return await db.query(`SELECT count(*) FROM likes WHERE post_id=${post_id}`);
+}
+
+export async function postCreator(post_id) {
+  return await db.query(`SELECT (user_id) FROM posts WHERE id=${post_id}`);
+}
+
+
+export async function postDelete(post_id) {
+  return await db.query(`DELETE FROM posts WHERE id=${post_id}`);
+}
